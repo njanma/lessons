85f58544ca59faad4d4670287b936400f63019c9
njanma <njanma1@gmail.com>
2018-12-01 19:53:50 +0300

    mini refactoring

--- src/main/java/_5_exceptions/App_23_generic_exception.java
+++ src/main/java/_5_exceptions/App_23_generic_exception.java
@@ -5,23 +5,34 @@ import java.util.logging.Logger;
 
 public class App_23_generic_exception {
 
+    public static void main(String[] args) {
+        throwAnException();
+
+//        throw new Exception();
+//        sneakyThrow(new Exception());
+    }
 //    class SomethingWrong<E> extends Exception{}
 
-    static <T extends Throwable> void throwAn(Class<T> clazz) {
+    static void throwAnException() {
         try {
             throw new IllegalArgumentException();
 //        } catch (T ex) {  // ОШИБКА:  перехватить переменную типа нельзя!
         } catch (Throwable ex) {
             Logger.getGlobal().log(Level.WARNING, "An exception was thrown: " + ex);
 
-            throwAs(ex, RuntimeException.class);
-//            throwAs(ex, Exception.class);
+            App_23_generic_exception.<RuntimeException>throwAs(ex);
+//            App_23_generic_exception.<Exception>throwAs(ex);
         }
     }
 
     @SuppressWarnings("unchecked")
-    private static <T extends Throwable> void throwAs(Throwable e, Class<T> cl) throws T {
+    private static <T extends Throwable> void throwAs(Throwable e) throws T {
         throw (T) e;  // Приведение обобщенного типа, стирается в тип  (Throwable)  е
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <T extends Throwable> void sneakyThrow(Throwable e) throws T {
+        throw (T) e;
     }
 }
 
