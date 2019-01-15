cf5965e4c1a1854b564bfbdebe6d5d76201f58b4
andrei.hryshanau <andrei.hryshanau@synesis-sport.by>
2019-01-15 10:47:25 +0300

    logic op refactoring

--- src/main/java/_1_procedural/App_1_1_logic_op.java
+++ src/main/java/_1_procedural/App_1_1_logic_op.java
@@ -2,12 +2,26 @@ package _1_procedural;
 
 public class App_1_1_logic_op {
     public static void main(String[] args) {
-        if (isTrue() | isTrue())
-            System.exit(0);
+        //eager
+        print(isTrue() | isFalse());  // true false == true
+        print(isTrue() & isFalse());  // true false == false
+        print(isFalse() & isFalse()); // false false == false
+        //lazy
+        print(isTrue() || isFalse());  // true == true
+        print(isFalse() && isFalse()); // false == false
     }
 
     static boolean isTrue() {
-        System.out.println("isTrue?");
+        System.out.print("true ");
         return true;
+    }
+
+    static boolean isFalse() {
+        System.out.print("false ");
+        return true;
+    }
+
+    static void print(boolean expression) {
+        System.out.println("== " + expression);
     }
 }
