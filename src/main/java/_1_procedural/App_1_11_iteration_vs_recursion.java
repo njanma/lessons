3bdb47effd150039eb05ec5ebc8560d9524b6d02
njanma <njanma1@gmail.com>
2019-01-20 21:04:45 +0300

    explain recursion

--- src/main/java/_1_procedural/App_1_11_iteration_vs_recursion.java
+++ src/main/java/_1_procedural/App_1_11_iteration_vs_recursion.java
@@ -6,8 +6,13 @@ public class App_1_11_iteration_vs_recursion {
 
     public static void main(String[] args) {
         int[] ints = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        invertRecursively(ints, 0);
+        invert(ints);
         System.out.println(Arrays.toString(ints));
+
+        //recursion
+//        System.out.println(factRecursion(3));
+//        invertRecursively(ints, 0);
+//        System.out.println(fibonacciRec(8));
     }
 
     //iteration
@@ -31,4 +36,27 @@ public class App_1_11_iteration_vs_recursion {
         }
     }
 
+    static int fibonacciRec(final int number) {
+        System.out.println("input arg: " + number);
+        if (number == 1 || number == 2) {
+            return 1;
+        } else {
+            int result = fibonacciRec(number - 1) + fibonacciRec(number - 2);
+            System.out.println("out from frame with result: " + result);
+            return result;
+        }
+    }
+
+    static int factRecursion(final int number) {
+        System.out.println("input arg: " + number);
+        if (number <= 1) {
+            System.out.println("Found base case!");
+            System.out.println("return 1");
+            return 1;
+        } else {
+            int recursiveCallResult = factRecursion(number - 1);
+            System.out.println("result from recursive call: " + recursiveCallResult + " * input: " + number);
+            return number * recursiveCallResult;
+        }
+    }
 }
