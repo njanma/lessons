df8826a5592843c39405dfb70df275b87b3f9c70
andrei.hryshanau <andrei.hryshanau@synesis-sport.by>
2019-01-28 16:58:27 +0300

    added varargs&generics example

---
+++ src/main/java/_3_generics/App_12_varargs_erasure.java
@@ -0,0 +1,31 @@
+package _3_generics;
+
+/**
+ * http://www.angelikalanger.com/GenericsFAQ/FAQSections/ParameterizedTypes.html
+ */
+public class App_12_varargs_erasure {
+    static <T> T[] method_1(T t1, T t2) {
+        return method_2(t1, t2);                       // unchecked warning
+    }
+
+    static <T> T[] method_2(T... args) {
+        return args;
+    }
+
+
+    public static void main(String... args) {
+        String[] strings = method_1("bad", "karma");     // ClassCastException
+    }
+
+//    Here is what the compiler generates:
+
+//    static Object[] method_1( Object t1, Object t2) {
+//        return method_2( new Object[] {t1, t2} );                   // unchecked warning
+//    }
+//    static Object[] method_2( Object[] args) {
+//        return args;
+//    }
+//    public static void main(String[] args) {
+//        String[] strings = (String[]) method_1("bad", "karma");       // ClassCastException
+//    }
+}
