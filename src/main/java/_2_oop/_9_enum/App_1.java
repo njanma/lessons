f77178d716654196f7326f0ad264074fa8c45fc0
andrei.hryshanau <andrei.hryshanau@synesis-sport.by>
2019-01-30 13:42:38 +0300

    upd enums

--- src/main/java/_2_oop/_9_enum/App_1.java
+++ src/main/java/_2_oop/_9_enum/App_1.java
@@ -6,19 +6,26 @@ public class App_1 {
 
     public static void main(String[] args) {
 
-        Color color = Color.RED;
+        Color color = getEnum();
 
         switch (color) {
             case RED:
+                break;
             case BLACK:
+                break;
             case GREEN:
+                break;
         }
 
         System.out.println(Arrays.toString(Color.values()));
     }
+
+    private static Color getEnum() {
+        return 10 % 2 == 0 ? Color.GREEN : Color.BLACK;
+    }
 }
 
-enum Color {
+enum Color /*extends EnumClassExtension implements EnumInterfaceExtension*/ {
     RED("R"), BLACK("B"), GREEN("G");
 
     private final String letter;
@@ -26,4 +33,10 @@ enum Color {
     Color(String letter) {
         this.letter = letter;
     }
+}
+
+class EnumClassExtension {
+}
+
+interface EnumInterfaceExtension {
 }
